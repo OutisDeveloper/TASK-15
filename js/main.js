e57b248c5911617c43577b67b266e1a234059058
OutisDeveloper <behruz234o@gmail.com>
2023-03-30 01:34:40 +0500

    last-action

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ const sidebarToggle = document.querySelector(".hamburger");
 sidebarToggle.addEventListener("click", () => {
   sidebar.classList.toggle("close");
 });
+
+const body = document.body;
+const modeToggle = document.querySelector("#light");
+
+modeToggle.addEventListener("click", () => {
+  body.classList.toggle("light");
+});
